92ec6c870f12e7c00a9c6232eed2c138f4987c7f
n5229 <oneikawebbbus@gmail.com>
2022-01-16 19:04:31 -0500

    WeaponsVault

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+
+// Used for wrapping a page component
+export const Screen = styled.div`
+  background-color: var(--primary);
+  background-image: ${({ image }) => (image ? `url(${image})` : "none")};
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  @media screen and (max-width: 500px) {
+  
+  }
+
+`;
+
+// Used for providing space between components
+export const SpacerXSmall = styled.div`
+  height: 8px;
+  width: 8px;
+`;
+
+// Used for providing space between components
+export const SpacerSmall = styled.div`
+  height: 16px;
+  width: 16px;
+`;
+
+// Used for providing space between components
+export const SpacerMedium = styled.div`
+  height: 40px;
+  width: 24px;
+`;
+
+// Used for providing space between components
+export const SpacerLarge = styled.div`
+  height: 25px;
+  width: 500px;
+`;
+
+// Used for providing a wrapper around a component
+export const Container = styled.div`
+  display: flex;
+  flex: ${({ flex }) => (flex ? flex : 0)};
+  flex-direction: ${({ fd }) => (fd ? fd : "column")};
+  justify-content: ${({ jc }) => (jc ? jc : "flex-start")};
+  align-items: ${({ ai }) => (ai ? ai : "flex-start")};
+  background-color: ${({ test }) => (test ? "pink" : "none")};
+  width: 100%;
+  margin-Top: -1px;
+  background-image: ${({ image }) => (image ? `url(${image})` : "none")};
+  background-size: cover;
+  background-position: center;
+  
+`;
+
+
+export const TextTitle = styled.p`
+  color: red;
+  font-size: 22px;
+  font-weight: 500;
+  line-height: 1.6;
+`;
+
+export const TextSubTitle = styled.p`
+  color: var(--primary-text);
+  font-size: 18px;
+  line-height: 1.6;
+`;
+
+export const TextDescription = styled.p`
+  color: var(--primary-text);
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 1.5;
+
+`;
+
+export const StyledClickable = styled.div`
+  :active {
+    opacity: 0.6;
+  }
+`;
+
